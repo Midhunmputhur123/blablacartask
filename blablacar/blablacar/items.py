a2374770437416e0ea3949525c513b7447a33f47
Midhun M <midhunmohanputhur@gmail.com>
2018-06-13 17:02:43 +0530

    items are changed

--- blablacar/blablacar/items.py
+++ blablacar/blablacar/items.py
@@ -8,7 +8,7 @@
 import scrapy
 
 
-class BlablacarItem(scrapy.Item):
+class blablacarItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     source = scrapy.Field()
